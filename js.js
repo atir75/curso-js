f6a3a9b6db5cd6eff3c359dffc52a13ce8496513
rita <maria.rita27012@gmail.com>
2022-07-28 10:41:09 -0300

    commit aula 5 AND

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-
+/* 
 var x = 5;
 var y = 10;
 
@@ -7,4 +7,22 @@ function or(){
 }
 
 alert(or()); 
+*/
+
+/*no OR um dos dois precisa ser verdadeiro para dar true*/
+
+
+
+
+var x = 5;
+var y = 10;
+
+function and(){
+    return x < 10 && y > 10;
+}
+
+alert(and());
+
+/*no AND ambas precisam ser verdadeiras para dar true*/ 
+
 
